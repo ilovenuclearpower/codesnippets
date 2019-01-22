a151a13bc4107be809128e9e3142abb89934c053
jaghadai <42021038+jaghadai@users.noreply.github.com>
2019-01-22 15:19:08 -0800

    Create dictionary.cs

---
+++ dictionary.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Version 1: create a Dictionary, then add 4 pairs to it.
+        var dictionary = new Dictionary<string, int>();
+        dictionary.Add("cat", 2);
+        dictionary.Add("dog", 1);
+        dictionary.Add("llama", 0);
+        dictionary.Add("iguana", -1);
+        // The dictionary has 4 pairs.
+        Console.WriteLine("DICTIONARY 1: " + dictionary.Count);
+        //Writes out the size of the second dictionary.
+
+        // Version 2: create a Dictionary with an initializer.
+        var dictionary2 = new Dictionary<string, int>()
+        {
+            { "cat", 2},
+            { "dog", 1},
+            { "llama", 0},
+            { "iguana", -1}
+        };
+        //Ideally this would be a one-liner. 
+        // This dictionary has 4 pairs too.
+        Console.WriteLine("DICTIONARY 2: " + dictionary2.Count);
+        //Writes out the size of the second dictionary.
+    }
+}
+// Taken from DotNetPerls: https://www.dotnetperls.com/dictionary
